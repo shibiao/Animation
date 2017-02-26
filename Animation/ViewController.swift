4ca54380501097450771134df5e354d1ee5b2b9d
WalkWithWind <956035125@qq.com>
2017-02-26 10:57:04 +0800

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Mac on 2017/2/26.
+//  Copyright © 2017年 shibiao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
